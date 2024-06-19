dc0eda2a4a6b57e37613d63b9a6b3f42c2c2fd81
joy2dworld <joy.stacktrek@gmail.com>
2024-06-19 12:23:16 +0800

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  const member = {
+    name: "Kanika",
+    role: "Software Engineer",
+    skills: ["JavaScript", "React.js", "Node.js"],
+    isJunior: false,
+  };
+  return member;
+}
